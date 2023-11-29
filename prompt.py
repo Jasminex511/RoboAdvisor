98ddc855e4929c65e38297dc4c413f9a24756247
jasmine <jasmine1999511@gmail.com>
2023-11-28 23:20:00 -0500

    modify prompts

--- prompt.py
+++ prompt.py
@@ -9,15 +9,14 @@ context = [{'role':'system', 'content':"""
             Imagine that your customer base is college students. \
             Here are some examples of questions, you can generate questions based on these: \
             What are your main financial goals? (Like paying off student loans, saving for short-term goals, planning a trip, etc.). \
-            Do you have a part-time or full-time job? What is your approximate annual income? \
-            How much money would you like to use for your investment? \
-            By when do you hope to achieve your main financial goals? \
-            Do you have any other financial goals or plans aside from your main ones? \
-            What is your comfort level with investment risk? (Low, medium, high) \
-            Do you currently have any investments? \
-            How experienced are you with investing? (Beginner, some experience, very experienced) \
+            Do you want to have any savings or funds for emergency? How much? \
+            What is your comfort level with investment risk? (very low,low, medium, high,very high) \
+            What is your expected rate of return for your investments? \
+            Which kind of investment period you are looking for?(1-3 years,3-5 years, over 5 years) \
             Is your investment plan short-term, medium-term, or long-term? \
-            Do you have any specific preferences or dislikes for certain types of investments or sectors? \
+            Which of the following industries and markets do you have a preference for, such as :Healthcare, Finance, Real Estate, Consumer Discretionary, Consumer Staples, 
+            Energy, Industrials, Materials, Communication Services, Utilities, International Markets, Emerging Markets, ESG, AI & Technology, Biotech.
+            And what is the preference level for that interest? You can answer: not interested, a little interested, moderately interested, very interested, extremely interested. \
             When you believe you asked all the questions you need, wrap it up with a response starting with 'Thank you for providing all the necessary information.'.\
             You must follow this rule: starting with your second answer, ask the clients questions that relate to the previous client's answers. \
             """}]  # accumulate messages
@@ -31,13 +30,26 @@ prompt = [{'role': 'system', 'content': """
             
             The review is delimited with triple backticks. \
             Format your response as a JSON object with \
-            "Money Budget", "Investment For", "Investment Plan", "Risk Tolerance" ,"Preferred Industry"as the keys.
+            "Money Budget", "Investment For", "Investment Plan", 
+            "Risk Tolerance" ,"Investment Period" , "Preferred Induestry" , "Industry Preference" ,"Emergency Money" as the keys.
             If the information isn't present, use "unknown" \
             as the value.
             
-            For the "Risk Tolerance":Transform the outcome "low" to "10% volatility",
-                                    Transform the outcome "medium" to "15% volatility",
-                                    Transform the outcome "high" to "20% volatility", \
+            For the "Risk Tolerance":Transform the outcome "very low" to "5% volatility",
+                                     Transform the outcome "low" to "10% volatility",
+                                     Transform the outcome "medium" to "15% volatility",
+                                     Transform the outcome "high" to "20% volatility",
+                                     Transform the outcome "very high" to "25% volatility",\
+            
+            For the "Ivestment Priod":  Transform the outcome "1-3 years" to "1-3 years with Volatility Adjustment Factor is 0.7",
+                                          Transform the outcome "3-5 years" to "3-5 years with Volatility Adjustment Factor is 1.0",
+                                          Transform the outcome "over 5 years" to "over 5 years with Volatility Adjustment Factor is 1.3",\
+            
+            For the "Industry Peference": Transform the outcome "not interested" to "0% weight",
+                                           Transform the outcome "a little interested" to "1%-10% weight",
+                                           Transform the outcome "moderately interested" to "11%-30% weight",
+                                           Transform the outcome "very interested" to "31%-50% weight",
+                                           Transform the outcome "extremely interested" to "51%-100% weight",\
             
             Make your response as short as possible. 
             """}]
